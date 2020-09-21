5ac0491da1ea2bef7598d050f30801aadc3a2036
wannaWhat <352782Tt>
2020-09-22 01:22:53 +0300

    Oh ho it's first commit

---
+++ wannaTerm.py
@@ -0,0 +1,16 @@
+from time import sleep
+import eel
+import keyboard
+
+
+
+# def wannaTerm():
+# 	while 1:
+# 		sleep(0.5)
+# 		if keyboard.is_pressed('ctrl+alt+S'):
+			
+
+if __name__ == "__main__":
+	eel.init("web")
+	eel.start('index.html')
+	# wannaTerm()
